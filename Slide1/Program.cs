6cd861e03f7fcac9341ac6bccade47d883ec63b6
xWasabiH <xindrah_95@hotmail.com>
2018-12-17 20:03:44 +0100

    Initial commit

---
+++ Slide1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Slide1
+{
+
+    class Program
+    {
+        static int sequential_search(int[] inputArray, int find)
+        //also called linear search
+        //Simplest, but least efficient
+        //Examines each element SEQUENTIALLY, from the first index to the last
+        {
+            for (int i = 0; i < inputArray.Length; i++) //loop through the inputArray
+            {
+                if (inputArray[i] == find)              //if the value on index i equals the value u want to "find"
+                {                                       // inputArray[i]  -> the value in the inputArray at index i 
+                    return i;                           //return 1 (found)
+                }
+            }
+            return -1;                                  //else return -1 (value not found)
+        }
+        static int binary_search(int[] inputArray, int find)
+        //standard search algorithm for a SORTED sequence
+        //*requires the order of elements 
+        //divide the sequence in two and FOCUSES on the half which could contain the element
+        {                                                   //example
+            int low = 0;                                    //low          middle           high
+            int high = inputArray.Length - 1;       //values//1 ,   2,3,   5      ,9,10,16, 20
+            while (low <= high)                     //index///0     1 2    3       4 5  6   7            
+            {
+                int middle = (low + high) / 2;      //middle = (0 + 7) / 2 = 3.5 -> 3       
+                if (find < inputArray[middle])      //if find is lower than 5
+                {
+                    high = middle - 1;              //assign "high" to 3 - 1 -> 2    !!index 2 NOT value 2
+                }
+                else if (find > inputArray[middle]) //if find is higher than 5
+                {
+                    low = middle + 1;               //assign "low"to 3 + 1 -> 4      !!index 4 NOT value 4 
+                }
+                else
+                {
+                    return middle;                  //if find == value at index middle -> return that value
+                }
+            }
+            return -1;                              //value not inputArray
+        }
+        static int binary_search_re(int[] list, int low, int high, int find)
+        //RECURSIVE
+        {
+
+            if (low > high)
+            {
+                return -1;
+            }
+            int middle = (low + high) / 2;
+            if (list[middle] > find)
+            {
+                return binary_search_re(list, low, middle - 1, find);
+            }
+            else if (list[middle] < find)
+            {
+                return binary_search_re(list, middle + 1, high, find);
+            }
+            else
+            {
+                return middle;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //int[] listone = { 1, 4, 7, 8, 9, 12, 15, 16, 20, 21, 25 };
+            int[] listtwo = { 2, 4, 7, 9, 10, 22, 27, 50, 77, 88, 100, 123, 155, 188 };
+
+            //System.Console.WriteLine("found on index: " + sequential_search(listone, 9));
+            //System.Console.WriteLine("found on index: " + binary_search_re(listtwo, 0, listtwo.Length - 1, 155));
+            System.Console.WriteLine("found on index: " + binary_search(listtwo, 188));
+        }
+    }
+
+}
---
+++ Slide2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace Slide2
+{
+    class Program
+    {
+        static void printArray(int[] a)
+        {
+            foreach (int i in a)
+            {
+                System.Console.WriteLine(i.ToString() + " ");
+            }
+        }
+        
+        static int[] insertion_sort(int[] a)
+        //from slide
+        //works
+        {
+            for (int j = 1; j < a.Length; j++)
+            {
+                var key = a[j];
+                var i = j - 1;
+                while (i >= 0 && a[i] > key)
+                {
+                    a[i + 1] = a[i];
+                    i = i - 1;
+                }
+                a[i + 1] = key;
+            }
+            return a;
+        }
+        static int[] BubbleSort(int[] inputArray)
+        {
+            int temp;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length - 1; j++)
+                {
+                    if (inputArray[j] > inputArray[j + 1])
+                    {
+                        temp = inputArray[j + 1];
+                        inputArray[j + 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+            return inputArray;
+        }
+
+        static int[] BubbleSortSlide(int[] inputArray)
+        //from slide
+        //fk this
+        {
+
+            for (int i = 1; i < inputArray.Length - 1; i++)
+            {
+                for (int j = inputArray.Length - 1; j < (i + 1); j++)
+                {
+                    if (inputArray[j] < inputArray[j - 1])
+                    {
+                        var temp = inputArray[j + 1];
+                        inputArray[j] = inputArray[j - 1];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+            return inputArray;
+        }
+        static int[] mergeSort(int[] array, int p, int r)
+        {
+            if (p < r)
+            {
+                var q = (p + r) / 2;
+                mergeSort(array, p, q);
+                mergeSort(array, q + 1, r);
+                merge(array, p, q, r);
+            }
+            return array;
+        }
+
+        static int[] merge(int[] array, int p, int q, int r)
+        {
+            var n_one = q - p + 1;
+            var n_two = r - 1;
+            int[] L = {};
+            int[] R = {};
+
+            for (int i = 0; i < n_one; i++)
+            {
+                L[i] = array[p + 1 - 1];
+            }
+            for (int j = 0; j < n_two; j++)
+            {
+                R[j] = array[q + j];
+            }
+            var a = 1;
+            var b = 1;
+            for (int k = p; k < r; r++)
+            {
+                if (L[a] <= R[b])
+                {
+                    array[k] = L[a];
+                    a = a + 1;
+                }
+                else
+                {
+                        array[k] = R[b];
+                        b = b + 1;
+                }
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            int[] unsortedlist = { 5, 2, 4, 6, 1, 3 };
+            int[] unsortedlistTwo = { 8,3,2,9,7,1,5,4};
+
+
+
+            int[] sortedlist = mergeSort(unsortedlistTwo,unsortedlistTwo[0],unsortedlistTwo[7]);
+            printArray(sortedlist);
+
+
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            System.Console.WriteLine("time: " + elapsedMs + "s");
+        }
+    }
+}
